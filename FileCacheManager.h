5264fb22466fc36e7bb1d4708cb25d6d9638e6e7
Aviwer <aviwer613@gmail.com>
2020-01-16 16:14:35 +0200

    16/1 16:15

--- FileCacheManager.h
+++ FileCacheManager.h
@@ -16,7 +16,7 @@ template<typename S>
 
 class FileCacheManager : CacheManager<string , S> {
     unordered_map<string , S> cache;
-    unordered_map<string , bool> ifExist;
+    unordered_map<string , string> ifExist;
     int sizeCache=0;
 
 public:
@@ -25,24 +25,45 @@ public:
         string str = hashString(problem);
         // insert to mapCache
         if(sizeCache==5) {
-            cache.erase(cache.end());
+            cache.erase(cache.end()-1);
             sizeCache--;
         }
         cache.insert({str, solution});
         sizeCache++;
 
         //insert to file cache
-        ifExist.insert({str, true});
+        ifExist.insert({problem, str});
+        str= str+".txt";
         std::ofstream toFile(str, std::ios::binary);
         toFile.write((char *) &solution, sizeof(solution));
         toFile.close();
     }
-
+/// get the solution from the cache
     S get(string problem) {
+        S solu;
 
+        //get from map cache
         if (cache.find(problem) != cache.end()) {
             return cache.find(problem);
         }
+
+        //get from file cache
+        string solutionName = ifExist.find(problem) + ".txt";
+        std::ifstream f(solutionName, std::ios::binary);
+        if (f.fail()) {
+            throw ("key not exist in files");
+        }
+        f.read((char*) &solu, sizeof(solu));
+        f.close();
+
+        //update the map cache
+        if(sizeCache==5) {
+            cache.erase(cache.end()-1);
+            sizeCache--;
+        }
+        cache.insert({ifExist.find(problem), solu});
+        sizeCache++;
+        return solu;
     }
 
     bool ifExistSolution(string problem) {
@@ -51,10 +72,6 @@ public:
             return false;
         }
         return true;
-    }
-
-     void saveSolution(string problem, S solution) {
-
     }
 
     string hashString(string str) {
